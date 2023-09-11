b4e099bbf3b26e1e46a0ad25c35c1ec0afd0b3dd
Wout De Maeseneer <wout.de.maeseneer@gmail.com>
2023-09-11 20:58:46 +0200

    hashlink

--- src/pages/homepage/Index.jsx
+++ src/pages/homepage/Index.jsx
@@ -1,6 +1,12 @@
 export default function Index() {
 
   return (
-    <p>test</p>
+    <>
+    <p className="mb-[300px]" id="intro">intro</p>
+    <p className="mb-[300px]" id="skills">test</p>
+    <p className="mb-[300px]" id="projects">test2</p>
+    <p className="mb-[500px]" id="aboutme">test3</p>
+    </>
+    
   );
 }
--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import W_logo_wit from '../../images/W_logo_wit.png';
 
 export default function Navbar() {
 
   return (
-    <nav class='bg-black text-white w-full flex flex-row'>
+    <nav class='bg-black text-white w-full flex flex-row px-5'>
       <div id='navbar_logo'>
-        <Link to='/'>
-          <p className='link_text'><img src={W_logo_wit} alt="W_logo" /></p>
-        </Link>
+        <HashLink class='px-5 font-semibold' smooth to='/#top' >
+          <img id='start' class='w-24' src={W_logo_wit} alt="W_logo" />
+        </HashLink>
+        {/* <Link to='/'>
+          <img class='w-24' src={W_logo_wit} alt="W_logo" />
+        </Link> */}
       </div>
-      <div id='navbar_link_container' class='flex flex-row'>
-        <Link to='/projects'>
-          <p className='link_text'>projects</p>
+      <div id='navbar_link_container' class='flex flex-row justify-center w-full items-center'>    
+        <HashLink class='px-5 font-semibold' smooth to='/#skills' > skills </HashLink>
+        <HashLink class='px-5 font-semibold' smooth to='/#projects' > projects </HashLink>
+        <HashLink class='px-5 font-semibold' smooth to='/#aboutme' > about me </HashLink>
+        <HashLink class='px-5 font-semibold' smooth to='/#timeline' > timeline </HashLink>
+        <HashLink class='px-5 font-semibold' smooth to='/#contact' > contact </HashLink>
+        {/* <Link to='/projects'>
+          <p class='px-5 font-semibold'>projects</p>
         </Link>
-        <Link to='/about me'>
-          <p className='link_text'>about me</p>
+        <Link to=''>
+          <p class='px-5 font-semibold'>about me</p>
         </Link>
-        <Link to='/timeline'>
-          <p className='link_text'>timeline</p>
+        <Link to=''>
+          <p class='px-5 font-semibold'>timeline</p>
         </Link>
-        <Link to='/contact'>
-          <p className='link_text'>contact</p>
-        </Link>
+        <Link to=''>
+          <p class='px-5 font-semibold'>contact</p>
+        </Link> */}
       </div>
     </nav>
   );
