5bcd6772a3d0d692029bd3d011d8fa5c5d8c8956
Wout De Maeseeneer <wout.de.maeseneer@gmail.com>
2024-08-13 22:58:55 +0200

    chore: change order inside skill component

--- src/pages/homepage/sections/Skills/Skills.jsx
+++ src/pages/homepage/sections/Skills/Skills.jsx
@@ -11,12 +11,11 @@ export default function Skills() {
 
     return (
         <div className='flex flex-col items-center'>
-            <div className="flex items-center justify-center bg-gray-700 bg-gray-900 rounded-full h-10 w-10 mb-10 animate-bounce select-none">
+            <div className="flex items-center justify-center bg-gray-700 bg-gray-900 rounded-full h-10 w-10 mb-14 animate-bounce select-none">
                 <img className='drop-shadow-glow h-6' src={arrowDown} alt="arrow-down"/>
             </div>
-            <h1 className="m-auto font-bold text-3xl mb-6">Skills<span className="text-[#D6F919]">.</span></h1>
-            <SkillsCarousel/>
-            <div className='flex flex-row my-10 hover:cursor-pointer select-none' onClick={() => setCanShowSkillRatings(!canShowSkillRatings)}>
+            <h1 className="m-auto font-bold text-3xl">Skills<span className="text-[#D6F919]">.</span></h1>
+            <div className='flex flex-row mb-10 mt-2 hover:cursor-pointer select-none' onClick={() => setCanShowSkillRatings(!canShowSkillRatings)}>
                 <p className='text-center font-bold'>View skill ratings</p>
                 <img id='start' className='drop-shadow-glow h-4 mt-1 ml-2' src={canShowSkillRatings ? dropUp : dropDown} alt="drop-down"/>
             </div>
@@ -25,6 +24,7 @@ export default function Skills() {
                     ? <SkillRatings/>
                     : null
             }
+            <SkillsCarousel/>
         </div>
     );
 }
