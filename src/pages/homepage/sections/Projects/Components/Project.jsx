3949098446c857ca782567f4ee39ce163eada8b3
Wout De Maeseeneer <wout.de.maeseneer@gmail.com>
2024-08-25 03:58:28 +0200

    feat: project tile bottom margin decrease

--- src/pages/homepage/sections/Projects/Components/Project.jsx
+++ src/pages/homepage/sections/Projects/Components/Project.jsx
@@ -2,7 +2,7 @@ import inProgressIcon from '../../../../../images/in-progress.png';
 
 export default function Project({ name, description, logo, inProgress }) {
   return (
-    <div className="flex justify-center items-center flex-[0_0_32%] mx-auto aspect-square mb-6 bg-neutral-900 overflow-hidden relative">
+    <div className="flex justify-center items-center flex-[0_0_32%] mx-auto aspect-square mb-3 bg-neutral-900 overflow-hidden relative">
       <img className="h-5/6 object-cover brightness-90" src={logo} alt="project-logo"/>
       {
         inProgress 
